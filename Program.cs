3d67124263723afe5415c247c8ba19f9ba24a3a7
RolandStrod <KhaleAldeAsad@gmail.com>
2020-09-10 12:24:27 +0300

    my commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+            Console.WriteLine("What is your favourite animal?");
+            UserInput = Console.ReadLine();
+
+            if (UserInput == "cat")
+            {
+                Console.WriteLine("You are a HomeBird.");
+            }
+            else if (UserInput == "dog")
+            {
+                Console.WriteLine("you are a partygoer.");
+            }
+            else
+            {
+                Console.WriteLine($"You are a {UserInput} freind");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }      
+                    
+                    
+              
+        }
+    }
